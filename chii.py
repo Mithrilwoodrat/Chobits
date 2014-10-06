ead81586b57ba06bcbbbd1d2124f72e1011f0d74
Mithrilwoodrat <lixiao766@qq.com>
2014-10-06 14:07:54 +0800

    change png

--- chii.py
+++ chii.py
@@ -5,7 +5,11 @@ from PyQt4 import  QtGui, QtCore
 from PyQt4.QtCore import Qt
 from PyQt4.QtGui import QMessageBox
 from PyQt4.QtGui import QPixmap
-from PyQt4.QtGui import QLabel
+from PyQt4.QtCore import QRect
+from PyQt4.QtGui import QImage
+from PyQt4.QtGui import QPainter
+
+
 
 try:
     _encoding = QtGui.QApplication.UnicodeUTF8
@@ -19,19 +23,31 @@ except AttributeError:
 class Chii(QtGui.QWidget):
     def __init__(self, parent=None):
         super(Chii, self).__init__()
-        self.picname = "chii.jpg"
-        self.picture = QLabel(self)
+        self.picnames = ["chii.png"]
+        self.pictures = []
+        self.current_pic = None
+        self.current_pic_num = 0
+        self.initui()
 
-        self.initUI()
-
-    def initUI(self):
+    def initui(self):
         self.setWindowFlags(QtCore.Qt.FramelessWindowHint)
         self.load_img()
         self.createContextMenu()
 
     def load_img(self):
-        self.picture.setGeometry(0,0,154,300)
-        self.picture.setPixmap(QPixmap(self.picname))
+        for picname in self.picnames:
+            self.pictures.append(QPixmap(picname))
+        self.current_pic = self.pictures[self.current_pic_num]
+        self.setMask(self.current_pic.mask())
+
+    def paintEvent(self, QPaintEvent):
+        self.resize(self.current_pic.width(),self.current_pic.height())
+        self.painter = QPainter()
+        self.painter.begin(self)
+        self.img = QImage(self.current_pic.toImage())
+        self.pic_rect = QRect(0, 0, self.current_pic.width(), self.current_pic.height())
+        self.painter.drawImage(self.pic_rect, self.img)
+        self.painter.end()
 
     def mousePressEvent(self, QMouseEvent):
         if QMouseEvent.buttons() == Qt.LeftButton:
@@ -50,14 +66,24 @@ class Chii(QtGui.QWidget):
         # create QMenu
         self.contextMenu = QtGui.QMenu(self)
         self.say_menu = self.contextMenu.addAction(u'Hai')
+        self.changpic_menu = self.contextMenu.addAction(u'changepicture')
         self.close_menu = self.contextMenu.addAction(u'close')
 
         self.say_menu.triggered.connect(self.say_action)
+        self.changpic_menu.triggered.connect(self.changpic)
         self.close_menu.triggered.connect(self.colse_action)
 
     def showContextMenu(self, pos):
         self.contextMenu.move(self.pos() + pos)
         self.contextMenu.show()
+
+    def changpic(self):
+        if self.current_pic_num < len(self.pictures)-1:
+            self.current_pic_num += 1
+        else:
+            self.current_pic_num = 0
+        self.current_pic = self.pictures[self.current_pic_num]
+        self.update()
 
     def colse_action(self):
         self.close()
